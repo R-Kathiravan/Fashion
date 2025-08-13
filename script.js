cf2d15249e433a8141a54a1811a33a57588b1158
R-Kathiravan <rajakathir988@gmail.com>
2025-08-13 18:17:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+ 
+document.querySelector(".hamburger").addEventListener("click", function () {
+    document.querySelector(".menu-items").classList.toggle("show");
+});
+ document.querySelector(".btn-close").addEventListener("click", function () {
+    document.querySelector(".menu-items").classList.remove("show");
+    });
+
+
+ let index = 0;
+const testimonials = document.querySelectorAll(".testimonial");
+const dots = document.querySelectorAll(".dot");
+
+function showTestimonial(n) {
+  testimonials.forEach((t, i) => {
+    t.classList.toggle("active", i === n);
+    dots[i].classList.toggle("active", i === n);
+  });
+}
+
+function nextTestimonial() {
+  index = (index + 1) % testimonials.length;
+  showTestimonial(index);
+}
+
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    index = i;
+    showTestimonial(index);
+  });
+});
+
+ setInterval(nextTestimonial, 4000);
+
+ showTestimonial(index);
+ 
