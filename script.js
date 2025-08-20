909fe4cebfe93d83afe270ada1e0e34633b95ede
R-Kathiravan <rajakathir988@gmail.com>
2025-08-20 14:00:20 +0530

    Updated Version

--- script.js
+++ script.js
@@ -36,4 +36,10 @@ dots.forEach((dot, i) => {
  
 
 
- 
+    //  const modal = document.getElementById("loginModal");
+    // const btn = document.getElementById("openLogin");
+    // const close = document.querySelector(".close");
+
+    // btn.onclick = () => modal.style.display = "flex";
+    // close.onclick = () => modal.style.display = "none";
+    // window.onclick = (e) => { if (e.target === modal) modal.style.display = "none"; }
