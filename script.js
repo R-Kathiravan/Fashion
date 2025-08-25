b3bfea07bed07ccd2b01b95c4d94d77ae5acbda9
R-Kathiravan <rajakathir988@gmail.com>
2025-08-25 17:23:49 +0530

    Add Services

--- script.js
+++ script.js
@@ -34,12 +34,3 @@ dots.forEach((dot, i) => {
 
  showTestimonial(index);
  
-
-
-    //  const modal = document.getElementById("loginModal");
-    // const btn = document.getElementById("openLogin");
-    // const close = document.querySelector(".close");
-
-    // btn.onclick = () => modal.style.display = "flex";
-    // close.onclick = () => modal.style.display = "none";
-    // window.onclick = (e) => { if (e.target === modal) modal.style.display = "none"; }
