b7d0a77d7b2ac4a534d5bb6f195a97c67091d949
R-Kathiravan <rajakathir988@gmail.com>
2025-09-01 18:15:25 +0530

    Added About2

--- Homepage2/script.js
+++ Homepage2/script.js
@@ -35,4 +35,21 @@ themeBtn.addEventListener("click", () => {
       themeBtn.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
     });
 
+    const backToTopBtn = document.getElementById("backToTop");
+
+   window.onscroll = function () {
+      if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopBtn.style.display = "block";
+      } else {
+        backToTopBtn.style.display = "none";
+      }
+    };
+
+     backToTopBtn.onclick = function () {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    };
+
  
